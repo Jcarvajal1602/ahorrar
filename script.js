29c93cdd0f6614e4119f773290cf18d4ee7bb56f
jcarvajal1602 <jahircarvajal1602@gmail.com>
2020-11-03 00:14:52 -0500

    primer commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-if('serviceWorker' in navigator){
-
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.log('Error al tratar de registrar el SW', err))
-
-}
+      .then(reg => console.log('Registro de SW exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el sw', err))
+  }
